5d096473e2f988b42c208297b0465c0e4f157798
vickyd885 <vickyd885@gmail.com>
2018-10-06 18:33:01 +0100

    fixing module loading bug

--- gcal_api/update_calendar.py
+++ gcal_api/update_calendar.py
@@ -8,4 +8,4 @@ import bsa_scraper
 if __name__ == '__main__':
     events = bsa_scraper.events_scraper.scrape_for_events()
     print("Number of events scrapped: ", len(events))
-    gcal_api.manipulate_events(events)
+    gcal_api.gcal_api.manipulate_events(events)
